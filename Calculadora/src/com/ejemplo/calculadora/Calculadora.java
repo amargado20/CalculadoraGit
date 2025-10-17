91126d6d7741ae631b88f38fb1a84699f01be97e
estudiante <estudiante@oretania-PC>
2025-10-17 10:04:05 +0200

    clase Calculadora con operación de suma

---
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package com.ejemplo.calculadora;
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a, b, total; 
+		String operacion;
+		
+		System.out.println("Escribe tu primer numero: ");
+		a = sc.nextDouble();
+		
+		System.out.println("Escribe tu segundo numero: ");
+		b = sc.nextDouble();
+		
+		System.out.println("Escribe el simbolo de la operacion que quieras hacer: ");
+		operacion = sc.next();
+		
+		switch (operacion) {
+		case "+":
+			total = sumar(a, b);
+			break;
+		case "-":
+			total = restar(a, b);
+			break;
+		case "*":
+			total = multiplicar(a, b);
+			break;
+		case "/":
+				total = dividir(a, b);
+				break;
+			} 
+		
+		
+		
+	}
+	
+		 public static double sumar(double a, double b) {
+		 return a + b;
+		 }
+		 
+
+}
